4f4ea46dab0ffe04d6eab4d0b4c7079d65ef1bb9
imblowfish <stagg7777@gmail.com>
2020-10-22 23:39:48 +0300

    adds operation Context class

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,41 @@
 #include <iostream>
 
+class Context {
+public:
+    uint16_t opcode;
+    uint16_t addr;
+    uint8_t nibble;
+    uint8_t x;
+    uint8_t y;
+    uint8_t kk;
+    uint8_t head;
+
+    Context(const uint16_t _opcode) {
+        opcode = _opcode;
+        head = (opcode & 0xF000) >> 12;
+        x = (opcode & 0x0F00) >> 8;
+        y = (opcode & 0x00F0) >> 4;
+        nibble = opcode & 0x000F;
+        kk = opcode & 0x00FF;
+        addr = opcode & 0x0FFF;
+    }
+
+    void print() {
+        printf( "==============\n"
+                "   Context:   \n"
+                "==============\n"
+                "opcode: 0x%04X\n"
+                "nibble: 0x%04X\n"
+                "x:      0x%04X\n"
+                "y:      0x%04X\n"
+                "kk:     0x%04X\n"
+                "head:   0x%04X\n"
+                "==============\n", 
+                opcode, nibble, 
+                x, y, kk, head );
+    }
+};
+
 int main() {
     std::cout<<"Hello chip8"<<std::endl;
     return 0;
