5548d32cef0e7e4e138d58261ef531a2b84d25b2
imblowfish <stagg7777@gmail.com>
2020-10-28 21:42:57 +0300

    adds clearScreen in display class adds draw in display class

--- include/display.h
+++ include/display.h
@@ -6,6 +6,9 @@ private:
     uint16_t buf[2048];
 public:
     Display();
+
+    void clearScreen();
+    bool draw(uint8_t x, uint8_t y, uint8_t row, uint8_t spriteLine);
 };
 
 #endif
--- src/display.cpp
+++ src/display.cpp
@@ -1,7 +1,21 @@
 #include <cstdint>
+#include <cstring>
 
 #include "display.h"
 
 Display::Display() : buf{0} {
 
+}
+
+void Display::clearScreen() {
+    memset(buf, 0, sizeof(buf));
+}
+
+bool Display::draw(uint8_t x, uint8_t y, uint8_t row, uint8_t spriteLine) {
+    (void)x;
+    (void)y;
+    (void)row;
+    (void)spriteLine;
+    // TODO: implement draw
+    return false;
 }
