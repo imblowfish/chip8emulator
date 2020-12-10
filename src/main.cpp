e654db2bde8f18823d76957acc68382019dd1f27
imblowfish <stagg7777@gmail.com>
2020-12-10 23:41:41 +0300

    adds printHelp in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,18 @@
 #include "chip8/memory.h"
 #include "chip8/processor.h"
 
+void printHelp() {
+    std::cout << "Chip8 emulator" << std::endl;
+    std::cout << "DESCRIPTION:" << std::endl;
+    std::cout << "\tsimple chip8 emulator with console display" << std::endl;
+    std::cout << "SYNOPSIS" << std::endl;
+    std::cout << "\tchip8 <chip8_rom_filepath>" << std::endl;
+}
+
 int main(int argc, char **argv) {
     if(argc != 2) {
-        std::cout << "Wrong arguments num" << std::endl;
+        std::cout << "Error: Wrong arguments num" << std::endl;
+        printHelp();
         return -1;
     }
     std::string filepath = argv[1];
